0412d5ee32bef055791fb73e1b60995bfd5b1229
sentidas <sentidas@yandex.ru>
2026-01-14 20:18:54 +0300

    test change rating2

--- country/src/main/java/ru/sentidas/country/service/VisitService.java
+++ country/src/main/java/ru/sentidas/country/service/VisitService.java
@@ -105,8 +105,8 @@ public class VisitService {
             entity.setArchived(visit.archived());
         }
         if (visit.rating() != null) {
-            if (visit.rating() < 1 || visit.rating() > 200) {
-                throw new BadRequestException("Rating must be between 1 and 200");
+            if (visit.rating() < 1 || visit.rating() > 300) {
+                throw new BadRequestException("Rating must be between 1 and 300");
             }
             entity.setRating(visit.rating());
         }
